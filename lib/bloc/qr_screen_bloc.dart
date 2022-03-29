771ca21fc12465a3ecef711a62f2097ca4e16030
Artsiom Muzychenka <artem8m@mail.ru>
2022-03-29 20:51:15 +0300

    added qr_reader

---
+++ lib/bloc/qr_screen_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../model/qr_screen_bloc_model.dart';
+
+class QrScreenBloc extends Cubit<QrScreenBlocState> {
+  QrScreenBloc(QrScreenBlocState initialState) : super(initialState);
+
+  
+}
